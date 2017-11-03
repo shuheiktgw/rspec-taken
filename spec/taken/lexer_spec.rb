e90bc2638c92d72c65ddf190a3de9def634cba49
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-11-03 11:26:12 +0900

    refactor lexer spec with shared examples

--- spec/taken/lexer_spec.rb
+++ spec/taken/lexer_spec.rb
@@ -5,6 +5,12 @@ require 'taken/reader'
 require 'taken/token'
 
 RSpec.describe Taken::Lexer do
+  RSpec.shared_examples 'assert lexing' do |token_type, literal, spaces|
+    it { expect(token.type).to eq(token_type)  }
+    it { expect(token.literal).to eq(literal)  }
+    it { expect(token.white_spaces).to eq(spaces)  }
+  end
+
   let(:lexer) { Taken::Lexer.new(reader) }
   let(:reader) { Taken::Reader.new(file) }
 
@@ -15,163 +21,123 @@ RSpec.describe Taken::Lexer do
     context '= is given' do
       let(:content) { '=' }
 
-      it { expect(token.type).to eq(Taken::Token::UNKNOWN)  }
-      it { expect(token.literal).to eq('=')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::UNKNOWN, '=', ''
     end
 
     context '== is given' do
       let(:content) { '==' }
 
-      it { expect(token.type).to eq(Taken::Token::EQ)  }
-      it { expect(token.literal).to eq('==')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::EQ, '==', ''
     end
 
     context '=== is given' do
       let(:content) { '===' }
 
-      it { expect(token.type).to eq(Taken::Token::UNKNOWN)  }
-      it { expect(token.literal).to eq('===')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::UNKNOWN, '===', ''
     end
 
     context '( is given' do
       let(:content) { '(' }
 
-      it { expect(token.type).to eq(Taken::Token::LPAREN)  }
-      it { expect(token.literal).to eq('(')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::LPAREN, '(', ''
     end
 
     context ') is given' do
       let(:content) { ')' }
 
-      it { expect(token.type).to eq(Taken::Token::RPAREN)  }
-      it { expect(token.literal).to eq(')')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::RPAREN, ')', ''
     end
 
     context '{ is given' do
       let(:content) { '{' }
 
-      it { expect(token.type).to eq(Taken::Token::LBRACE)  }
-      it { expect(token.literal).to eq('{')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::LBRACE, '{', ''
     end
 
     context '} is given' do
       let(:content) { '}' }
 
-      it { expect(token.type).to eq(Taken::Token::RBRACE)  }
-      it { expect(token.literal).to eq('}')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::RBRACE, '}', ''
     end
 
     context ': is given' do
       let(:content) { ':' }
 
-      it { expect(token.type).to eq(Taken::Token::COLON)  }
-      it { expect(token.literal).to eq(':')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::COLON, ':', ''
     end
 
     context 'EOF is given' do
       let(:content) { '' }
 
-      it { expect(token.type).to eq(Taken::Token::EOF)  }
-      it { expect(token.literal).to eq('EOF')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::EOF, 'EOF', ''
     end
 
     context 'do is given' do
       let(:content) { 'do' }
 
-      it { expect(token.type).to eq(Taken::Token::DO)  }
-      it { expect(token.literal).to eq('do')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::DO, 'do', ''
     end
 
     context 'end is given' do
       let(:content) { 'end' }
 
-      it { expect(token.type).to eq(Taken::Token::END_KEY)  }
-      it { expect(token.literal).to eq('end')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::END_KEY, 'end', ''
     end
 
     context 'When is given' do
       let(:content) { 'When' }
 
-      it { expect(token.type).to eq(Taken::Token::WHEN)  }
-      it { expect(token.literal).to eq('When')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::WHEN, 'When', ''
     end
 
     context 'Then is given' do
       let(:content) { 'Then' }
 
-      it { expect(token.type).to eq(Taken::Token::THEN)  }
-      it { expect(token.literal).to eq('Then')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::THEN, 'Then', ''
     end
 
     context 'Given is given' do
       let(:content) { 'Given' }
 
-      it { expect(token.type).to eq(Taken::Token::GIVEN)  }
-      it { expect(token.literal).to eq('Given')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::GIVEN, 'Given', ''
     end
 
     context 'Given is given' do
       let(:content) { 'Given!' }
 
-      it { expect(token.type).to eq(Taken::Token::GIVEN_BANG)  }
-      it { expect(token.literal).to eq('Given!')  }
-      it { expect(token.white_spaces).to eq('')  }
+      it_behaves_like 'assert lexing', Taken::Token::GIVEN_BANG, 'Given!', ''
     end
 
     context 'identifiers are given' do
       context 'normal' do
         let(:content) { 'identifier' }
 
-        it { expect(token.type).to eq(Taken::Token::IDENT)  }
-        it { expect(token.literal).to eq('identifier')  }
-        it { expect(token.white_spaces).to eq('')  }
+        it_behaves_like 'assert lexing', Taken::Token::IDENT, 'identifier', ''
       end
 
       context 'with _' do
         let(:content) { '_some_identifier' }
 
-        it { expect(token.type).to eq(Taken::Token::IDENT)  }
-        it { expect(token.literal).to eq('_some_identifier')  }
-        it { expect(token.white_spaces).to eq('')  }
+        it_behaves_like 'assert lexing', Taken::Token::IDENT, '_some_identifier', ''
       end
 
       context 'with -' do
         let(:content) { 'some-identifier' }
 
-        it { expect(token.type).to eq(Taken::Token::IDENT)  }
-        it { expect(token.literal).to eq('some-identifier')  }
-        it { expect(token.white_spaces).to eq('')  }
+        it_behaves_like 'assert lexing', Taken::Token::IDENT, 'some-identifier', ''
       end
 
       context 'with CAPITAL LETTERS' do
         let(:content) { 'CAPITAL_LETTERS' }
 
-        it { expect(token.type).to eq(Taken::Token::IDENT)  }
-        it { expect(token.literal).to eq('CAPITAL_LETTERS')  }
-        it { expect(token.white_spaces).to eq('')  }
+        it_behaves_like 'assert lexing', Taken::Token::IDENT, 'CAPITAL_LETTERS', ''
       end
     end
 
     context 'white spaces are given' do
       let(:content) { " \n\t\r" }
 
-      it { expect(token.type).to eq(Taken::Token::EOF)  }
-      it { expect(token.literal).to eq('EOF')  }
-      it { expect(token.white_spaces).to eq(" \n\t\r")  }
+      it_behaves_like 'assert lexing', Taken::Token::EOF, 'EOF', " \n\t\r"
     end
   end
 end
