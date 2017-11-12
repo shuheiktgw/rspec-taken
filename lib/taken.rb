d4dfa047c330c40e31c24dead8cee02e674e5e9a
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-11-12 17:11:08 +0900

    implemented taken class

--- lib/taken.rb
+++ lib/taken.rb
@@ -1,5 +1,42 @@
-require "taken/version"
+require 'taken/loader'
+require 'taken/reader'
+require 'taken/lexer'
+require 'taken/parser'
+require 'taken/writer'
+require 'taken/generator'
 
-module RspecTaken
+module Taken
+  class << self
+    def taken(path)
+      while loader(path).load_next_file
+        generator.execute
+      end
+    end
 
+    private
+
+    def loader(path)
+      @loader ||= Loader.new(path)
+    end
+
+    def reader
+      Reader.new(loader.file)
+    end
+
+    def lexer
+      Lexer.new(reader)
+    end
+
+    def parser
+      Parser.new(lexer)
+    end
+
+    def writer
+      Writer.new(loader.current_file_name)
+    end
+
+    def generator
+      Generator.new(parser, writer)
+    end
+  end
 end
