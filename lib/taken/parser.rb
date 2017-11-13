9a0334e0d5717547f62aac89d30c0c2e629f075f
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-11-13 10:43:19 +0900

    implementing then parser

--- lib/taken/parser.rb
+++ lib/taken/parser.rb
@@ -19,6 +19,8 @@ module Taken
       parsed = case current_token.type
       when Token::GIVEN
         parse_given
+      when Token::THEN
+        parse_then
       when Token::EOF
         Ast::EOF.new
       else
@@ -45,6 +47,40 @@ module Taken
       end
 
       Ast::GivenDeclaration.new(spaces: spaces, keyword: keyword)
+    end
+
+    def parse_then
+      spaces = current_token.white_spaces
+      block = parse_then_block
+    end
+
+    def parse_then_block
+      opener = current_token
+
+      get_next
+
+      sentences = parse_then_sentence
+      while current_token.type != select_closer(opener)
+        sentences << parse_then_sentence
+      end
+
+      get_next
+
+      sentences
+    end
+
+    def parse_then_sentence
+
+    end
+
+    def select_closer(opener)
+      if opener.type == Token::LBRACE
+        Token::RBRACE
+      elsif opener.type == Token::DO
+        Token::END
+      else
+        raise "Unknown opener is specified: #{opener.literal}"
+      end
     end
 
     def get_next
