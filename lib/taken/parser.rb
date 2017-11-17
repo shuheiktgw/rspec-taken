fa0217fe5e4f912d7a527aab475813a5f2dc04bb
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-11-17 10:39:21 +0900

    implemented parse then sentence

--- lib/taken/parser.rb
+++ lib/taken/parser.rb
@@ -59,9 +59,9 @@ module Taken
 
       get_next
 
-      sentences = parse_then_sentence
+      sentences = parse_then_sentence(opener)
       while opener.block_closer?(current_token)
-        sentences << parse_then_sentence
+        sentences << parse_then_sentence(opener)
       end
 
       get_next
@@ -69,9 +69,29 @@ module Taken
       sentences
     end
 
-    def parse_then_sentence
+    def parse_then_sentence(opener)
+      tokens = []
+      eq_count = 0
 
+      until current_token.newline? || opener.block_closer?(current_token)
+        tokens << current_token
+        eq_count += 1 if current_token.type == Token::EQ
 
+        get_next
+      end
+
+      form_then_sentence tokens, eq_count
+    end
+
+    def form_then_sentence(tokens, eq_count)
+      if eq_count == 0
+        Ast::Then::NormalSentence.new(tokens)
+      elsif eq_count == 1
+        idx = tokens.index{ |token| token.type == Token::EQ }
+        Ast::Then::AssertionSentence.new(left: tokens[0...idx], right: tokens[idx+1..-1])
+      else
+        raise "Cannot parse then statement with more than two == tokens. got: #{eq_count}"
+      end
     end
 
     def get_next
