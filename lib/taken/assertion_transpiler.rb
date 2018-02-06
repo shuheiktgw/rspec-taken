800dd3b806c7af07b599a32ecb4abf0bfe4b6891
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-02-06 10:56:03 +0900

    fix error with handling error message

--- lib/taken/assertion_transpiler.rb
+++ lib/taken/assertion_transpiler.rb
@@ -6,12 +6,8 @@ class Taken::AssertionTranspiler
       when /^\s*expect.+$/
         sentence
       # do_something == Failure(SomeError, /message/) => expect{ do_something }.to raise_error(SomeError, message)
-      when /^\s*(\S+)\s*==\s*Failure\(?([A-Za-z]+),\s*\/(.+)\/\s*\)?\s*$/
-        if $3 == 'empty'
-          "expect{ #{$1} }.to raise_error(#{$2})"
-        else
-          "expect{ #{$1} }.to raise_error(#{$2}, '#{$3}')"
-        end
+      when /^\s*(\S+)\s*==\s*Failure\(?([A-Za-z]+),\s*([^)]+)\s*\)?\s*$/
+        "expect{ #{$1} }.to raise_error(#{$2}, #{$3})"
       # 1 == 1 => expect(1).to eq(1)
       # TODO: Handle double equals
       when /^\s*(\S+)\s*==\s*(\S+)\s*$/
--- spec/taken/assertions_transpiler_spec.rb
+++ spec/taken/assertions_transpiler_spec.rb
@@ -16,17 +16,27 @@ RSpec.describe Taken::AssertionTranspiler do
       it { is_expected.to eq 'expect(1).to eq(1)' }
     end
 
-    context 'failure sentence' do
-      let(:sentence) {" do_something == Failure(SomeError, /#{message}/) "}
+    context 'failure sentence with an error type and a message' do
+      let(:sentence) {" do_something == Failure(SomeError, #{message}) "}
 
-      context 'empty message' do
-        let(:message) { 'empty' }
-        it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError)" }
+      context 'regex message' do
+        let(:message) { '/Some error occurred!/' }
+        it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError, /Some error occurred!/)" }
       end
 
-      context 'not empty message' do
-        let(:message) { 'Some error occurred!' }
+      context 'single quote message' do
+        let(:message) { "'Some error occurred!'" }
         it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError, 'Some error occurred!')" }
+      end
+
+      context 'double quote message' do
+        let(:message) { '"Some error occurred!"' }
+        it { is_expected.to eq 'expect{ do_something }.to raise_error(SomeError, "Some error occurred!")' }
+      end
+
+      context 'message is a variable' do
+        let(:message) { 'message' }
+        it { is_expected.to eq 'expect{ do_something }.to raise_error(SomeError, message)' }
       end
     end
   end
