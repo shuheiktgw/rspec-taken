7ca0b57269dbbd16eb87fbe983f2b359c3263073
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-02-09 10:52:49 +0900

    Enable to transpile Failure without parenthesis

--- lib/taken/assertion_transpiler.rb
+++ lib/taken/assertion_transpiler.rb
@@ -1,20 +1,34 @@
 class Taken::AssertionTranspiler
   class << self
+    # TODO: Handle double equals
     def transpile(sentence)
       case sentence
       # expect(1).to eq(1) => expect(1).to eq(1)
       when /^\s*expect.+$/
         sentence
       # do_something == Failure(SomeError, /message/) => expect{ do_something }.to raise_error(SomeError, message)
-      when /^\s*(\S+)\s*==\s*Failure\(?([A-Za-z]+),\s*([^)]+)\s*\)?\s*$/
-        "expect{ #{$1} }.to raise_error(#{$2}, #{$3})"
+      when /^\s*(.+)\s*==\s*Failure\(\s*([A-Za-z]+)\s*,\s*(.+)\s*\)\s*$/
+        "expect{ #{ cleanup_sentence $1 } }.to raise_error(#{ cleanup_sentence $2 }, #{ cleanup_sentence $3 })"
+      # do_something == Failure SomeError, /message/ => expect{ do_something }.to raise_error(SomeError, message)
+      when /^\s*(.+)\s*==\s*Failure\s*([A-Za-z]+)\s*,\s*(.+)\s*$/
+        "expect{ #{ cleanup_sentence $1 } }.to raise_error(#{ cleanup_sentence $2 }, #{ cleanup_sentence $3 })"
       # 1 == 1 => expect(1).to eq(1)
-      # TODO: Handle double equals
-      when /^\s*(\S+)\s*==\s*(\S+)\s*$/
-        "expect(#{$1}).to eq(#{$2})"
+      when /^\s*(.+)\s*==(.+)$/
+        "expect(#{ cleanup_sentence $1 }).to eq(#{ cleanup_sentence $2 })"
       else
-        "expect(#{sentence}).to be_truthy"
+        "expect(#{ cleanup_sentence sentence }).to be_truthy"
       end
+    end
+
+    private
+
+    def cleanup_sentence(sentence)
+      striped = sentence.strip
+      while striped.start_with?('(') && striped.end_with?(')')
+        striped.slice!(0)
+        striped.slice!(-1)
+      end
+      striped
     end
   end
 end
--- spec/taken/assertions_transpiler_spec.rb
+++ spec/taken/assertions_transpiler_spec.rb
@@ -12,31 +12,64 @@ RSpec.describe Taken::AssertionTranspiler do
     end
 
     context 'normal == sentence' do
-      let(:sentence) {' 1 == 1 '}
-      it { is_expected.to eq 'expect(1).to eq(1)' }
+      context 'without parenthesis' do
+        let(:sentence) {' 1 == 1 '}
+        it { is_expected.to eq 'expect(1).to eq(1)' }
+      end
+
+      context 'with parenthesis' do
+        let(:sentence) {' 1 == (1 + (1 + (1)) - 2) '}
+        it { is_expected.to eq 'expect(1).to eq(1 + (1 + (1)) - 2)' }
+      end
     end
 
     context 'failure sentence with an error type and a message' do
-      let(:sentence) {" do_something == Failure(SomeError, #{message}) "}
+      context 'with parenthesis' do
+        let(:sentence) {" do_something == Failure( SomeError, #{message}) "}
 
-      context 'regex message' do
-        let(:message) { '/Some error occurred!/' }
-        it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError, /Some error occurred!/)" }
+        context 'regex message' do
+          let(:message) { '/Some error occurred! (test!)/' }
+          it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError, /Some error occurred! (test!)/)" }
+        end
+
+        context 'single quote message' do
+          let(:message) { "'Some error occurred! (test!)'" }
+          it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError, 'Some error occurred! (test!)')" }
+        end
+
+        context 'double quote message' do
+          let(:message) { '"Some error occurred! (test!)"' }
+          it { is_expected.to eq 'expect{ do_something }.to raise_error(SomeError, "Some error occurred! (test!)")' }
+        end
+
+        context 'message is a variable' do
+          let(:message) { 'message(test!)' }
+          it { is_expected.to eq 'expect{ do_something }.to raise_error(SomeError, message(test!))' }
+        end
       end
 
-      context 'single quote message' do
-        let(:message) { "'Some error occurred!'" }
-        it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError, 'Some error occurred!')" }
-      end
+      context 'without parenthesis' do
+        let(:sentence) {" do_something == Failure SomeError, #{message} "}
 
-      context 'double quote message' do
-        let(:message) { '"Some error occurred!"' }
-        it { is_expected.to eq 'expect{ do_something }.to raise_error(SomeError, "Some error occurred!")' }
-      end
+        context 'regex message' do
+          let(:message) { '/Some error occurred! (test!)/' }
+          it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError, /Some error occurred! (test!)/)" }
+        end
 
-      context 'message is a variable' do
-        let(:message) { 'message' }
-        it { is_expected.to eq 'expect{ do_something }.to raise_error(SomeError, message)' }
+        context 'single quote message' do
+          let(:message) { "'Some error occurred! (test!)'" }
+          it { is_expected.to eq "expect{ do_something }.to raise_error(SomeError, 'Some error occurred! (test!)')" }
+        end
+
+        context 'double quote message' do
+          let(:message) { '"Some error occurred! (test!)"' }
+          it { is_expected.to eq 'expect{ do_something }.to raise_error(SomeError, "Some error occurred! (test!)")' }
+        end
+
+        context 'message is a variable' do
+          let(:message) { 'message(test!)' }
+          it { is_expected.to eq 'expect{ do_something }.to raise_error(SomeError, message(test!))' }
+        end
       end
     end
   end
