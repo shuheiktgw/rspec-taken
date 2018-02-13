793b3d59a58d41053689814a9f20a3893db724ce
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-02-13 10:06:41 +0900

    Add more fancy matcher translaions

--- lib/taken/assertion_transpiler.rb
+++ lib/taken/assertion_transpiler.rb
@@ -12,6 +12,15 @@ class Taken::AssertionTranspiler
       # do_something == Failure SomeError, /message/ => expect{ do_something }.to raise_error(SomeError, message)
       when /^\s*(.+)\s*==\s*Failure\s*([A-Za-z]+)\s*,\s*(.+)\s*$/
         "expect{ #{ cleanup_sentence $1 } }.to raise_error(#{ cleanup_sentence $2 }, #{ cleanup_sentence $3 })"
+      # nil.nil? => expect(nil).to be_nil?
+      when /^(.+)\.([^.\s]+)\?\s*$/
+        "expect(#{ cleanup_sentence $1 }).to be_#{ cleanup_sentence $2 }"
+      # false == false => expect(false).to be_falsey
+      when /^\s*(.+)\s*==\s*false\s*$/
+        "expect(#{ cleanup_sentence $1 }).to be_falsey"
+      # true != false => expect(false).not_to eq(false)
+      when /^\s*(.+)\s*!=\s*(.+)\s*$/
+        "expect(#{ cleanup_sentence $1 }).not_to eq(#{ cleanup_sentence $2 })"
       # 1 == 1 => expect(1).to eq(1)
       when /^\s*(.+)\s*==(.+)$/
         "expect(#{ cleanup_sentence $1 }).to eq(#{ cleanup_sentence $2 })"
--- spec/taken/assertions_transpiler_spec.rb
+++ spec/taken/assertions_transpiler_spec.rb
@@ -72,5 +72,34 @@ RSpec.describe Taken::AssertionTranspiler do
         end
       end
     end
+    context 'end_with ?' do
+      context 'valid?' do
+        let(:sentence) {" some_model.valid? "}
+        it { is_expected.to eq 'expect(some_model).to be_valid' }
+      end
+
+      context 'empty?' do
+        let(:sentence) {" [1, 2, 3, 4].empty? "}
+        it { is_expected.to eq 'expect([1, 2, 3, 4]).to be_empty' }
+      end
+
+      context 'nil?' do
+        let(:sentence) {" (nil + nil + nil + nil).nil? "}
+        it { is_expected.to eq 'expect(nil + nil + nil + nil).to be_nil' }
+      end
+
+      context 'include?' do
+        let(:sentence) {" [1, 2, 3, 4, 5].include?(1) "}
+        it { is_expected.not_to eq 'expect([1, 2, 3, 4, 5]).to include?(1)' }
+      end
+    end
+    context '== false' do
+      let(:sentence) {" false == false "}
+      it { is_expected.to eq 'expect(false).to be_falsey' }
+    end
+    context '!=' do
+      let(:sentence) {" false != true "}
+      it { is_expected.to eq 'expect(false).not_to eq(true)' }
+    end
   end
 end
