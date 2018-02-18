e0b71ce2aed6ffc8413a0e2b272f8f9753ce38b9
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-02-18 09:20:07 +0900

    fix gsub! to gsub

--- lib/taken/writer.rb
+++ lib/taken/writer.rb
@@ -4,7 +4,7 @@ module Taken
     attr_reader :file, :new_file_path
 
     def initialize(file_path:)
-      @new_file_path = file_path.gsub!(/_spec.rb/, '_taken_spec.rb')
+      @new_file_path = file_path.gsub(/_spec.rb/, '_taken_spec.rb')
       raise "Invalid file path is given. file_path: #{file_path}" if @new_file_path.nil?
 
       @file = File.open(@new_file_path, 'w')
